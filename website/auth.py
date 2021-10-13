a44b0d98b37d343a576dcc8560f2fd9384b26a81
razvanvilceanu <Sidewinder13>
2021-10-14 00:13:40 +0300

    added html templates

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing", user="Razvan")
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -13,6 +15,22 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+    if len(email) < 4:
+        flash('Email must be greater than 4 characters', category='error')
+    elif len(first_name) < 2:
+        pass
+    elif password1 != password2:
+        pass
+    elif len(password1) < 7:
+        pass
+    # else:
+
     return render_template("sign_up.html")
